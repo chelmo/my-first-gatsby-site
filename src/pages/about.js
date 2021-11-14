17490a74979886b80457b09472950d9a058844f6
Chelmo <chelmo@gmail.com>
2021-11-14 00:30:05 -0800

    adding css layout files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,14 @@
 // Step 1: Import React
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <Link to="/">Back to home</Link>
-            <p>Hi!</p>
-        </main>
+        <Layout pageTitle="About Me">
+       <p>Hi, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+    </Layout>
     )
 }
 
