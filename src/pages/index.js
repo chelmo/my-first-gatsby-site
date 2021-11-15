cbdcedb708c43c85ba0c46879ed18b60078f82bb
Chelmo <chelmo@gmail.com>
2021-11-14 19:19:21 -0800

    added image plugins, uploaded screenshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
        <p>I'm making this by following the Gatsby Tutorial.</p>
+       <StaticImage
+       alt="Gatsby tutorial page for adding plugins to your application"
+       src="../images/screenshot.png"
+       />
     </Layout>
   )
 }
