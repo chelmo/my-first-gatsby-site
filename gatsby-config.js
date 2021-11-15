1d4619e533e76ba915cf27562a33124739fb48cb
Chelmo <chelmo@gmail.com>
2021-11-14 19:28:04 -0800

    edited syntax error

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: "My First Gatsby Site",
   },
   plugins: [
-          "gatsby-plugin-image",
-          "gatsby-plugin-sharp",
+          'gatsby-plugin-image',
+          'gatsby-plugin-sharp',
     ],
 }
