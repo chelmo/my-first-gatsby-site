1601f8e8781569129d780b850bdadbe98de4508e
Chelmo <chelmo@gmail.com>
2021-11-17 20:10:12 -0800

    renamed blog to index.js within blog dir

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+import { title } from 'process'
+import { date } from 'joi'
+
+// ...
+
+const BlogPost = ({ data }) => {
+    return (
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
+            </Layout>
+    )
+}
+
+export const query = graphql`
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+            body
+        }
+    }
+`
+
+
+
+
+export default BlogPost
